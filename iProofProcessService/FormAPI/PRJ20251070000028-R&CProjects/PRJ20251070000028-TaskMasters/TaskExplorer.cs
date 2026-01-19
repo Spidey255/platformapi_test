fddc81639dc9e9b3bb5cdc3adf54239369779b79
Perumalbs <perumal.bs92@gmail.com>
2026-01-19 11:48:40 +0530

    latest build

--- iProofProcessService/FormAPI/PRJ20251070000028-R&CProjects/PRJ20251070000028-TaskMasters/TaskExplorer.cs
+++ iProofProcessService/FormAPI/PRJ20251070000028-R&CProjects/PRJ20251070000028-TaskMasters/TaskExplorer.cs
@@ -799,6 +799,7 @@ ISpace["RedirectUrl"].Value="?PkActMId=5EC89147-2FE2-40AF-8446-350FE5E13C57&frmE
 base.WriteDebugInfo(@"
 ISpace[""RedirectUrl""].RedirectType=""R"";
 ISpace[""RedirectUrl""].Value=""?PkActMId=5EC89147-2FE2-40AF-8446-350FE5E13C57&frmElementId=74DE8FDB-50E6-4EF1-BA80-FBD2FF5FD49A&PkPrMId=3cfe092a-c9df-42bd-bb7a-d4a6e90379fc&formVersionId=430A1903-C481-4EA5-BDD0-0C7F581A5215&Ver=0.0000&MF_w1_task_id=""+ISpace[""MF_TaskId""].Value;");
+
 }
 catch(Exception ex)
 {
