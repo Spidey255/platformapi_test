d46057dd5352fe439a10b46cfa8a87c4b09679f4
Perumalbs <perumal.bs92@gmail.com>
2026-01-21 12:25:33 +0530

    new

---
+++ iProofProcessService/Interface/IExtElementBase.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPS.Proof.DFSExtension
+{
+    public interface IExtElementBase
+    {
+        string GetInsertFormDataQuery(string instanceId, string packageProcessMapId, string processActivityMapId,
+          string formId, long usermapid, string formJsonData);
+
+        string GetInsertGridDataQuery(string instanceId, string packageProcessMapId, string processActivityMapId,
+          string formId, long usermapid, string formJsonData);
+
+        string GetGridLoopQuery(string gridId, string gridName, Dictionary<string, ServiceElementData> gridData);
+    }
+}
